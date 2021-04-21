946e39f970493548a0c050382aefa821af0bf385
Paulo <hst.paulo@gmail.com>
2021-04-21 19:00:26 -0300

    readme

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">Podcastr</h1>
+<!-- <h1 align="center">
+  <img alt="" title="" src="" width="400px"/>
+</h1> -->
+
+## Objetivo
+...
+
+## Tecnologias / Ferramentas / Bibliotecas
+- JavaScript
+- TypeScript
+- Next.js
+- Sass
+- Node.js
+
+## Status
+- [ ] Em andamento
+
+## Links
+- [Rocketseat](https://rocketseat.com.br/)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,9 +21,12 @@ export async function getStaticProps() {
 }
 
 // aula1: #missaoespacial
+// aula2: #embuscadoproximonivel
+
 // json-server: api fake para testes (-w: watch / -d: delay / -p: porta)
 
 // SPA: Dificulta o SEO porque os crawlers não esperam o carregamento total da API
 // SSR: Utiliza-se a função getServerSideProps para resolver o problema acima. executa TODA vez que tem acesso á pagina
 // SSG: Evita carregamento toda vez que acessa a página, evita requisições desnecessárias. mais performance
+
 // revalidade: tempo de regarregamento, gerando uma nova versão da página (nova chamada API)
